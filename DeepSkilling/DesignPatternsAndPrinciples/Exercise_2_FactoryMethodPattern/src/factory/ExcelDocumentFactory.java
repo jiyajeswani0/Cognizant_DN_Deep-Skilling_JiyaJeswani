9f864f04956042369087a67ac59db3f2fc45ef4e
jiyajeswani0 <jiyajeswani25@gmail.com>
2026-07-22 21:29:29 +0530

    Add Exercise 2 Factory Method Pattern

---
+++ DeepSkilling/DesignPatternsAndPrinciples/Exercise_2_FactoryMethodPattern/src/factory/ExcelDocumentFactory.java
@@ -0,0 +1,12 @@
+package factory;
+
+/**
+ * Concrete factory for creating ExcelDocument instances.
+ */
+public class ExcelDocumentFactory extends DocumentFactory {
+
+    @Override
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
